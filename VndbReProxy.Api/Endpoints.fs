829ddee50e054bf2d931d6f65354b8b88ad72e85
Vladislav Prekel <misterptits@yandex.ru>
2021-09-07 19:35:46 +0700

    Add Giraffe.QueryReader #2

--- VndbReProxy.Api/Endpoints.fs
+++ VndbReProxy.Api/Endpoints.fs
@@ -2,25 +2,25 @@
 
 open Giraffe
 open Giraffe.EndpointRouting
-open Microsoft.AspNetCore.Http
+open Giraffe.QueryReader
 
 let private (=>) a b = a [ b ]
 
 let handler1: HttpHandler =
-    fun (_: HttpFunc) (ctx: HttpContext) -> ctx.WriteTextAsync "Hello World"
+    fun _ ctx -> ctx.WriteTextAsync "Hello World"
 
 let handler2 (firstName: string, age: int) : HttpHandler =
-    fun (_: HttpFunc) (ctx: HttpContext) ->
+    fun _ ctx ->
         sprintf "Hello %s, you are %i years old." firstName age
         |> ctx.WriteTextAsync
 
 let handler3 (a: string, b: string, c: string, d: int) : HttpHandler =
-    fun (_: HttpFunc) (ctx: HttpContext) ->
+    fun _ ctx ->
         sprintf "Hello %s %s %s %i" a b c d
         |> ctx.WriteTextAsync
 
 let endpoints =
-    [ GET => route "/" (text "Hello World")
+    [ GET => route "/" ^ Query.read ("to", text)
       GET => routef "/%s/%i" handler2
       GET => routef "/%s/%s/%s/%i" handler3
       subRoute
