05e92dc9148ae1d62b7278110b02f8f95ad442c8
Vladislav Prekel <misterptits@yandex.ru>
2022-06-24 00:54:27 +0700

    .

--- VndbReProxy.Api/Program.fs
+++ VndbReProxy.Api/Program.fs
@@ -2,14 +2,17 @@ namespace VndbReProxy.Api
 
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
+open System
 
 module Program =
     let CreateHostBuilder args =
         Host
             .CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(fun webBuilder ->
-                webBuilder.UseStartup<Startup>()
-                |> ignore<IWebHostBuilder>)
+                webBuilder
+                    .UseStartup<Startup>()
+                    .UseUrls("http://*:" + (Environment.GetEnvironmentVariable("PORT")))
+                    |> ignore<IWebHostBuilder>)
 
     [<EntryPoint>]
     let main args =
